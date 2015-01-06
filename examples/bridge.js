bae00a038ee95405226b260b8d4a2385351eb91d
Stéphane GUILLY <stephane.guilly@gmail.com>
2015-01-06 20:54:57 +0100

    use ido4pro-logger

--- examples/bridge.js
+++ examples/bridge.js
@@ -2,23 +2,12 @@
  * Created by sguilly on 21/11/14.
  */
 
-
-var PrettyStream = require('bunyan-prettystream');
-
-var prettyStdOut = new PrettyStream();
-prettyStdOut.pipe(process.stdout);
+var logger = require('ido4pro-logger');
+logger.createLogger({name: "zibaseToMqtt", level: 'trace', console: true, path : __dirname});
 
 var mqttZibase = require('../lib/mqtt-zibase');
 
 var opts = {
-  logger: {
-    name: 'mqtt-zibase-bridge',
-    streams: [{
-      level: 'info',
-      type: 'raw',
-      stream: prettyStdOut
-    }]
-  },
   zibase: {
     ip: '192.168.0.18',
     port: 17100 // tcp,
@@ -26,9 +15,11 @@ var opts = {
   },
   mqtt: {
     ip : '188.213.25.148',
-    port: 3001, // tcp
+    port: 1883, // tcp
     topic: 'mqtt-zibase/ZiBASE0051b0',
-    clientId : 'ZiBASE0051b0'
+    clientId : 'ZiBASE0051b0',
+    username: 'zibaseToMqtt',
+    password: '****'
   }
 };
 
